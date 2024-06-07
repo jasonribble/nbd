2c17f18eec74c343b9e27a0b072ecdbfd07d187e
Jason Ribble <jasonribble@protonmail.com>
2024-06-06 19:06:35 -0700

    feat: create Contact model

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+
+#[derive(Debug)]
+struct Contact {
+  _first_name: String,
+  _last_name: String,
+  _display_name: String,
+  _email: String,
+  _phone_number: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let person = Contact {
+        _first_name: String::from("Jason"),
+        _last_name: String::from("Ribble"),
+        _display_name: String::from("Jason Ribble"),
+        _email: String::from("example@.com"),
+        _phone_number: String::from("123-456-7890"),
+    };
+
+    println!("{:?}", person);
 }
