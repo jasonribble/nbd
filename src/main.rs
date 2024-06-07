9f6c08a2b5148ccadc9d437fa27a30367ad5d622
Jason Ribble <jasonribble@protonmail.com>
2024-06-07 16:55:16 -0700

    feat: display name start creation

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
 #[derive(Debug)]
 struct Contact {
-    first_name: String,
-    last_name: String,
-    _display_name: String,
+    _first_name: String,
+    _last_name: String,
+    display_name: String,
     _email: String,
     _phone_number: String,
 }
 
 impl Contact {
-    fn new(first_name: String, last_name: String, display_name: String, email: String, phone_number: String) -> Result<Contact, String> {
-        if first_name.to_lowercase() == "ERROR" {
-            return Err("First name cannot be 'ERROR'".to_string());
-        }
+    fn new(first_name: String, last_name: String, email: String, phone_number: String) -> Result<Contact, String> {
+        let display_name = format!("{} {}", first_name, last_name);
 
         Ok(Contact {
-            first_name,
-            last_name,
-            _display_name: display_name,
+            _first_name: first_name,
+            _last_name: last_name,
+            display_name,
             _email: email,
             _phone_number: phone_number,
         })
@@ -24,16 +22,31 @@ impl Contact {
 }
 
 fn main() {
-    let person_result = Contact::new(
+    let person = Contact::new(
         String::from("Jason"),
         String::from("Ribble"),
         String::from("Jason Ribble"),
         String::from("example@.com"),
-        String::from("123-456-7890"),
-    );
+    ).unwrap();
 
-    match person_result {
-        Ok(person) => println!("Hi, my name is {} {}", person.first_name, person.last_name),
-        Err(error) => println!("Error creating contact: {}", error),
+    println!("Hi, my name is {}", person.display_name);
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_display_name() {
+        let person = Contact::new(
+            String::from("Jason"),
+            String::from("Ribble"),
+            String::from("example@.com"),
+            String::from("123-456-7890"),
+        ).unwrap();
+
+        let display_name = "Jason Ribble".to_string();
+        assert_eq!(person.display_name, display_name)
     }
 }
