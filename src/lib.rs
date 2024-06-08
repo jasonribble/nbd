06e7d9fa202a740ec678ad5055dbf45360914f71
Jason Ribble <git@j6r.org>
2024-06-08 10:52:54 -0700

    refactor: validation logic in it's own file

---
+++ src/lib.rs
@@ -0,0 +1,113 @@
+pub mod validation {
+    use regex::Regex;
+
+    pub fn is_valid_phone_number(phone: &str) -> bool {
+        let phone_regex = Regex::new(
+            r"^\+?1?\s*(\(\d{3}\)|\d{3})[-.\s]*\d{3}[-.\s]*\d{4}(?:\s*(?:ext|x|ex)\.?\s*\d+)?$",
+        )
+        .unwrap();
+        phone_regex.is_match(phone)
+    }
+
+    pub fn is_valid_email(email: &str) -> bool {
+        let pattern = r"^[\w\d][-\w\d+.]*@((?:[-\w\d]+\.)+[-\w\d]{2,})$";
+        let email_regex = regex::Regex::new(pattern).unwrap();
+        email_regex.is_match(email)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::validation::{is_valid_email, is_valid_phone_number};
+
+    #[test]
+    fn test_valid_phone_numbers() {
+        let valid_numbers = [
+            "1234567890",
+            "123-456-7890",
+            "123.456.7890",
+            "(123) 456-7890",
+            "+1 (123) 456-7890",
+            "123-456-7890 ext. 1234",
+            "123.456.7890 x1234",
+            "(123) 456-7890 ex 1234",
+            "1234567890x123",
+        ];
+
+        for number in &valid_numbers {
+            assert!(
+                is_valid_phone_number(number),
+                "Valid number '{}' failed validation",
+                number
+            );
+        }
+    }
+
+    #[test]
+    fn test_invalid_phone_numbers() {
+        let invalid_numbers = [
+            "123456789",
+            "1234567890123",
+            "123-456-789",
+            "(123 456-7890",
+            "123.456.78901",
+            "123-456-7890 ext",
+            "123.456.7890 x",
+            "(123) 456-7890 ex abc",
+        ];
+
+        for number in &invalid_numbers {
+            assert!(
+                !is_valid_phone_number(number),
+                "Invalid number '{}' passed validation",
+                number
+            );
+        }
+    }
+
+    #[test]
+    fn test_valid_email() {
+        let valid_emails = [
+            "john@example.com",
+            "john.doe@example.com",
+            "john_doe@example.co.uk",
+            "john123@example.com",
+            "john.doe+newsletter@example.com",
+            "a@meow.com",
+        ];
+
+        for valid_email in valid_emails {
+            assert!(
+                is_valid_email(valid_email),
+                "Valid email '{}' passed validation",
+                valid_email
+            )
+        }
+    }
+
+    #[test]
+    fn test_invalid_email() {
+        let invalid_emails = [
+            "john@example",
+            "john.example.com",
+            "john@.com",
+            "john@example..com",
+            "john@example.c",
+            "john@example.com.",
+            "john doe@example.com",
+            " lohn doe@example.com",
+            "john@example.com!",
+            "jaasdf asdf @.com",
+            ".test@example.com",
+            "+test@example.com",
+        ];
+
+        for invalid_email in invalid_emails {
+            assert!(
+                !is_valid_email(invalid_email),
+                "Invalid email '{}' passed validation",
+                invalid_email
+            )
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
+use companion_connect::validation::{is_valid_email, is_valid_phone_number};
+
 use std::fmt::Display;
-
-use regex::Regex;
-
 #[derive(Debug)]
 struct Contact {
     _first_name: String,
@@ -49,20 +48,6 @@ impl Display for Email {
     }
 }
 
-fn is_valid_phone_number(phone: &str) -> bool {
-    let phone_regex = Regex::new(
-        r"^\+?1?\s*(\(\d{3}\)|\d{3})[-.\s]*\d{3}[-.\s]*\d{4}(?:\s*(?:ext|x|ex)\.?\s*\d+)?$",
-    )
-    .unwrap();
-    phone_regex.is_match(phone)
-}
-
-fn is_valid_email(email: &str) -> bool {
-    let pattern = r"^[\w\d][-\w\d+.]*@((?:[-\w\d]+\.)+[-\w\d]{2,})$";
-    let email_regex = regex::Regex::new(pattern).unwrap();
-    email_regex.is_match(email)
-}
-
 impl Contact {
     fn new(first_name: String, last_name: String, email: String, phone_number: String) -> Self {
         let display_name = format!("{first_name} {last_name}");
@@ -106,96 +91,5 @@ mod tests {
         );
         let display_name = "Jason Ribble".to_string();
         assert_eq!(person.display_name, display_name)
-    }
-
-    #[test]
-    fn test_valid_phone_numbers() {
-        let valid_numbers = [
-            "1234567890",
-            "123-456-7890",
-            "123.456.7890",
-            "(123) 456-7890",
-            "+1 (123) 456-7890",
-            "123-456-7890 ext. 1234",
-            "123.456.7890 x1234",
-            "(123) 456-7890 ex 1234",
-            "1234567890x123",
-        ];
-
-        for number in &valid_numbers {
-            assert!(
-                is_valid_phone_number(number),
-                "Valid number '{}' failed validation",
-                number
-            );
-        }
-    }
-
-    #[test]
-    fn test_invalid_phone_numbers() {
-        let invalid_numbers = [
-            "123456789",
-            "1234567890123",
-            "123-456-789",
-            "(123 456-7890",
-            "123.456.78901",
-            "123-456-7890 ext",
-            "123.456.7890 x",
-            "(123) 456-7890 ex abc",
-        ];
-
-        for number in &invalid_numbers {
-            assert!(
-                !is_valid_phone_number(number),
-                "Invalid number '{}' passed validation",
-                number
-            );
-        }
-    }
-
-    #[test]
-    fn test_valid_email() {
-        let valid_emails = [
-            "john@example.com",
-            "john.doe@example.com",
-            "john_doe@example.co.uk",
-            "john123@example.com",
-            "john.doe+newsletter@example.com",
-            "a@meow.com",
-        ];
-
-        for valid_email in valid_emails {
-            assert!(
-                is_valid_email(valid_email),
-                "Valid email '{}' passed validation",
-                valid_email
-            )
-        }
-    }
-
-    #[test]
-    fn test_invalid_email() {
-        let invalid_emails = [
-            "john@example",
-            "john.example.com",
-            "john@.com",
-            "john@example..com",
-            "john@example.c",
-            "john@example.com.",
-            "john doe@example.com",
-            " lohn doe@example.com",
-            "john@example.com!",
-            "jaasdf asdf @.com",
-            ".test@example.com",
-            "+test@example.com",
-        ];
-
-        for invalid_email in invalid_emails {
-            assert!(
-                !is_valid_email(invalid_email),
-                "Invalid email '{}' passed validation",
-                invalid_email
-            )
-        }
     }
 }
