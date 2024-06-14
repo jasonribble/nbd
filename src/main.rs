143633fdc0a522d17306d7dc34cfcadd5704afe1
Jason Ribble <jasonribble@protonmail.com>
2024-06-13 21:07:05 -0700

    feat: dialoguer to get info

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
+use dialoguer::Input;
+
 mod contact;
 use crate::contact::Contact;
 
 fn main() {
-    let person = Contact::new(
-        String::from("Jason"),
-        String::from("Ribble"),
-        String::from("john@example.com"),
-        String::from("123-456-7890"),
-    );
+    println!("Welcome. Below insert the contact information");
 
-    println!("Hi, my name is {}", person.display_name);
-    println!("My phone number is {}", person.phone_number);
-    println!("My email is {}", person.email);
+    let first_name = Input::new()
+        .with_prompt("First name")
+        .interact_text()
+        .unwrap();
+
+    let last_name = Input::new()
+        .with_prompt("Last name")
+        .interact_text()
+        .unwrap();
+    
+    let email = Input::new()
+      .with_prompt("Email")
+      .interact_text()
+      .unwrap();
+
+    let phone = Input::new()
+        .with_prompt("Phone")
+        .interact_text()
+        .unwrap();
+
+    let person = Contact::new(first_name, last_name, email, phone);
+
+    println!();
+    println!("Contact name: {}", person.display_name);
+    println!("Contact number: {}", person.phone_number);
+    println!("Contact email {}", person.email);
 }
