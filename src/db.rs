30ef76d2572c68878bfa49b5c32972b0ebe3dbc4
Jason Ribble <jasonribble@protonmail.com>
2024-07-05 23:42:04 -0700

    feat: return id from save contact

--- src/db.rs
+++ src/db.rs
@@ -7,7 +7,7 @@ use sqlx::postgres::PgPool;
 #[cfg_attr(test, mockall::automock)]
 #[async_trait]
 pub trait ContactRepo {
-    async fn save_contact(&self, contact: models::Contact) -> anyhow::Result<()>;
+    async fn save_contact(&self, contact: models::Contact) -> anyhow::Result<i64>;
 }
 
 pub struct PostgresContactRepo {
@@ -24,21 +24,22 @@ impl PostgresContactRepo {
 
 #[async_trait]
 impl ContactRepo for PostgresContactRepo {
-    async fn save_contact(&self, contact: models::Contact) -> anyhow::Result<()> {
+    async fn save_contact(&self, contact: models::Contact) -> anyhow::Result<i64> {
         let query = "INSERT INTO contacts
-            (first_name, last_name, display_name, email, phone_number)
-            VALUES ($1, $2, $3, $4, $5)";
+        (first_name, last_name, display_name, email, phone_number)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING id";
 
-        sqlx::query(query)
+        let (id,) = sqlx::query_as::<_, (i64,)>(query)
             .bind(&contact.first_name)
             .bind(&contact.last_name)
             .bind(&contact.display_name)
             .bind(&contact.email)
             .bind(&contact.phone_number)
-            .execute(&*self.pg_pool)
+            .fetch_one(&*self.pg_pool)
             .await?;
 
-        Ok(())
+        Ok(id)
     }
 }
 
@@ -64,10 +65,12 @@ mod tests {
             .expect_save_contact()
             .times(1)
             .with(eq(test_contact.clone()))
-            .returning(|_| Ok(()));
+            .returning(|_| Ok(1));
 
         let result = mock_contact_repo.save_contact(test_contact).await;
 
-        assert!(result.is_ok());
+        let result = result.unwrap();
+
+        assert_eq!(result, 1);
     }
 }
