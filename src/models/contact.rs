63f348e263d8fc396f018a605bdce2832a406cae
Jason Ribble <jasonribble@protonmail.com>
2024-07-08 12:54:28 -0700

    refactor: simplify creating Contact + db query

--- src/models/contact.rs
+++ src/models/contact.rs
@@ -1,6 +1,6 @@
 use crate::{errors::AppError, utils};
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, sqlx::FromRow)]
 pub struct Contact {
     pub first_name: String,
     pub last_name: String,
@@ -9,35 +9,36 @@ pub struct Contact {
     pub phone_number: String,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, sqlx::FromRow)]
 pub struct ContactWithId {
     pub id: i64,
+    #[sqlx(flatten)]
     pub contact: Contact,
 }
 
 impl Contact {
     pub fn new(
-        first_name: String,
-        last_name: String,
-        email: String,
-        phone_number: String,
+        first_name: &str,
+        last_name: &str,
+        email: &str,
+        phone_number: &str,
     ) -> Result<Self, AppError> {
         let display_name = format!("{first_name} {last_name}");
 
-        if utils::is_not_valid_email(&email) {
-            return Err(AppError::InvalidEmail(email));
+        if utils::is_not_valid_email(email) {
+            return Err(AppError::InvalidEmail(email.to_owned()));
         }
 
-        if utils::is_not_valid_phone_number(&phone_number) {
-            return Err(AppError::InvalidPhoneNumber(phone_number));
+        if utils::is_not_valid_phone_number(phone_number) {
+            return Err(AppError::InvalidPhoneNumber(phone_number.to_owned()));
         }
 
         Ok(Self {
-            first_name,
-            last_name,
+            first_name: first_name.to_owned(),
+            last_name: last_name.to_owned(),
             display_name,
-            email,
-            phone_number,
+            email: email.to_owned(),
+            phone_number: phone_number.to_owned(),
         })
     }
 }
@@ -47,12 +48,7 @@ mod tests {
 
     #[test]
     fn test_display_name() {
-        let person = Contact::new(
-            String::from("Jason"),
-            String::from("Ribble"),
-            String::from("john@example.com"),
-            String::from("123-456-7890"),
-        );
+        let person = Contact::new("Jason", "Ribble", "john@example.com", "123-456-7890");
         let display_name = "Jason Ribble".to_string();
 
         assert_eq!(person.unwrap().display_name, display_name)
