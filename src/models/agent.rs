5b89ed17d5b59f88dd768ec3711d6d065962c002
Jason Ribble <jasonribble@protonmail.com>
2024-09-16 09:43:32 -0700

    feat: agent to string
    This may be helpful in the future

--- src/models/agent.rs
+++ src/models/agent.rs
@@ -1,9 +1,12 @@
+use std::fmt;
 struct Agent { 
+
     pub first_name: String
 }
 
 #[derive(Default)]
 struct AgentBuilder {
+    #[allow(dead_code)]
    pub first_name: Option<String>
 }
 
@@ -14,15 +17,23 @@ impl Agent {
 }
 
 impl AgentBuilder {
+    #[allow(dead_code)]
     pub fn first_name(mut self, first_name: &str) -> Self {
         self.first_name = Some(String::from(first_name));
         self
     } 
 
+    #[allow(dead_code)]
     pub fn build(self) -> Result<Agent, &'static str> {
         Ok(Agent {
             first_name: self.first_name.ok_or("_")?
         })
+    }
+}
+
+impl fmt::Display for Agent {       
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {   
+        write!(f, "{}", self.first_name)
     }
 }
 
@@ -36,6 +47,13 @@ mod tests {
         let agent = Agent::builder().first_name("Jason").build();
         
         assert!(agent.is_ok())
+    }
+
+    #[test]
+    fn test_agent_name_to_string() {
+        let agent = Agent::builder().first_name("Jason").build().unwrap();
+
+        assert_eq!(agent.to_string(), "Jason")
     }
 
 }
