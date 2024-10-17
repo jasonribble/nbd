7bc463b2f30b31940fe9019c13a5eec452cf2b49
Jason Ribble <jasonribble@protonmail.com>
2024-10-17 11:17:20 -0700

    refactor: call health check endpoint "ok"

--- tests/api.rs
+++ tests/api.rs
@@ -3,13 +3,13 @@ mod tests {
     use reqwest;
 
     #[tokio::test]
-    async fn test_hello_endpoint() {
-        let url = "http://localhost:8080/hello";
+    async fn test_ok_endpoint() {
+        let url = "http://localhost:8080/ok";
         let response = reqwest::get(url).await.expect("Failed to send request");
 
         assert_eq!(response.status(), 200);
 
         let body = response.text().await.expect("Failed to get response text");
-        assert_eq!(body, "Hello, World!");
+        assert_eq!(body, "ok");
     }
 }
