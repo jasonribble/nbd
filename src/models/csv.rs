e319c1b51c4663d866c1c16e9854295028a8a5bd
Jason Ribble <jasonribble@protonmail.com>
2025-03-21 12:35:40 -0700

    feat: read_csv fails when file not found

--- src/models/csv.rs
+++ src/models/csv.rs
@@ -5,7 +5,11 @@ fn read_csv(filename: &str) -> anyhow::Result<Vec<&str>> {
     let extension = path.extension().and_then(|ext| ext.to_str());
 
     match extension {
-        Some("csv") => Ok(Vec::new()),
+        Some("csv") => {
+            File::open(filename)
+                .map_err(|_| anyhow::anyhow!("Failed to open file: {}", filename))?;
+            Ok(Vec::new())
+        }
         _ => Err(anyhow::anyhow!("File must have .csv extension")),
     }
 }
@@ -30,5 +34,18 @@ mod tests {
         let result = read_csv(temp_csv.path().to_str().unwrap());
 
         assert!(result.is_ok());
+    }
+
+    #[test]
+    fn should_return_error_when_file_not_found() {
+        let non_exisistent_file_path = "non_existent.csv";
+
+        let result = read_csv(non_exisistent_file_path);
+
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            "Failed to open file: non_existent.csv"
+        );
     }
 }
