f061a2c2c6ab29cf1df10a2b4651e0791b18b41f
Jason Ribble <jasonribble@protonmail.com>
2025-03-28 16:40:09 -0700

    build(db): allow null values in contacts
    This will make things more flexible in the code. Only thing required is
    first_name

--- src/db/fake_db.rs
+++ src/db/fake_db.rs
@@ -43,15 +43,6 @@ pub mod test_helpers {
 
         Ok(first_entry)
     }
-
-    pub fn last_sql_snapshot() -> anyhow::Result<String> {
-        let entries = get_migration_entries()?;
-
-        let last_entry = &entries[entries.len() - 1];
-        let first_entry = String::from(last_entry.to_string_lossy());
-
-        Ok(first_entry)
-    }
 }
 
 #[cfg(test)]
@@ -61,7 +52,7 @@ mod tests {
 
     #[test]
     fn current_number_of_migrations() {
-        let migrations = 3;
+        let migrations = 4;
 
         let entries = get_migration_entries().unwrap();
 
@@ -82,48 +73,6 @@ phone_number TEXT NOT NULL
 );"#;
 
         let file_path = first_sql_snapshot().unwrap();
-
-        println!("{:?}", file_path);
-
-        let contents =
-            fs::read_to_string(file_path).expect("Should have been able to read the file");
-
-        assert_eq!(contents, expect_migration);
-    }
-
-    #[test]
-    fn read_last_sql_file_in_migrations() {
-        let expect_migration = r#"-- Add migration script here
--- Enable foreign key support
-PRAGMA foreign_keys = OFF;
-
--- Create a temporary table for contacts_metadata
-CREATE TABLE contacts_metadata_temp (
-    contact_id INTEGER PRIMARY KEY,
-    starred BOOLEAN NOT NULL,
-    is_archived BOOLEAN NOT NULL,
-    created_at TEXT NOT NULL,
-    updated_at TEXT NOT NULL,
-    last_seen_at TEXT,
-    next_reminder_at TEXT,
-    frequency INTEGER,
-    last_reminder_at TEXT,
-    FOREIGN KEY (contact_id) REFERENCES contacts(id) ON DELETE CASCADE
-);
-
--- Copy data from the old table to the new one
-INSERT INTO contacts_metadata_temp SELECT * FROM contacts_metadata;
-
--- Drop the old table
-DROP TABLE contacts_metadata;
-
--- Rename the new table to the original name
-ALTER TABLE contacts_metadata_temp RENAME TO contacts_metadata;
-
--- Re-enable foreign key support
-PRAGMA foreign_keys = ON;"#;
-
-        let file_path = last_sql_snapshot().unwrap();
 
         println!("{:?}", file_path);
 
