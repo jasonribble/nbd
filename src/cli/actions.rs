ab743dfafdcb0bdf2c42cb8b3ec0a059853d1e9b
Jason Ribble <jasonribble@protonmail.com>
2025-04-03 09:23:45 -0700

    refactor: create an Actions struct
    I'm not sure, it seems useful. We'll see

--- src/cli/actions.rs
+++ src/cli/actions.rs
@@ -1,106 +1,102 @@
 use nbd::{
-    db::{self, ContactRepo},
+    db::{self, Connection, ContactRepo},
     models::{self, ContactBuilder},
 };
 use tabled::Table;
 
 use crate::commander::{CreateCommand, DeleteCommand, EditCommand, GetCommand, ImportCommand};
 
-pub async fn create_contact(
-    command: &CreateCommand,
-    data_repo: &db::Connection,
-) -> Result<(), anyhow::Error> {
-    let contact = models::Contact::new(
-        command.first_name.as_deref().unwrap_or(""),
-        command.last_name.as_deref().unwrap_or(""),
-        command.email.as_deref().unwrap_or(""),
-        command.phone_number.as_deref().unwrap_or(""),
-        chrono::NaiveDate::default(),
-    );
-
-    let contact = contact.unwrap();
-
-    let id = data_repo.save_contact(contact).await?;
-
-    println!("Successfully saved contact {id}");
-
-    Ok(())
+pub struct Actions {
+    data_repo: db::Connection,
 }
 
-pub async fn edit_contact(
-    command: &EditCommand,
-    data_repo: &db::Connection,
-) -> Result<(), anyhow::Error> {
-    let contact = ContactBuilder::new(
-        command.id,
-        command.first_name.clone(),
-        command.last_name.clone(),
-        command.display_name.clone(),
-        command.email.clone(),
-        command.phone_number.clone(),
-    )
-    .unwrap();
-
-    let _ = data_repo.update_contact(contact).await;
-
-    Ok(())
-}
-
-pub async fn show_all_contacts(data_repo: &db::Connection) -> Result<(), anyhow::Error> {
-    let contacts = data_repo.get_all_contacts().await?;
-
-    if contacts.is_empty() {
-        println!("No contacts yet!");
-    } else {
-        let table = Table::new(contacts);
-        println!("{table}");
+impl Actions {
+    pub fn new(data_repo: Connection) -> Self {
+        Self { data_repo }
     }
 
-    Ok(())
-}
+    pub async fn create_contact(&self, command: &CreateCommand) -> Result<(), anyhow::Error> {
+        let contact = models::Contact::new(
+            command.first_name.as_deref().unwrap_or(""),
+            command.last_name.as_deref().unwrap_or(""),
+            command.email.as_deref().unwrap_or(""),
+            command.phone_number.as_deref().unwrap_or(""),
+            chrono::NaiveDate::default(),
+        );
 
-pub async fn get_contact(
-    command: &GetCommand,
-    data_repo: &db::Connection,
-) -> Result<(), anyhow::Error> {
-    let id = command.id;
+        let contact = contact.unwrap();
 
-    let contact = data_repo.get_contact_by_id(id).await?;
+        let id = self.data_repo.save_contact(contact).await?;
 
-    println!("{contact:?}");
+        println!("Successfully saved contact {id}");
 
-    Ok(())
-}
-
-pub async fn delete_contact(
-    command: &DeleteCommand,
-    data_repo: &db::Connection,
-) -> Result<(), anyhow::Error> {
-    let id = command.id;
-
-    let contact_id = data_repo.delete_contact_by_id(id).await?;
-
-    println!("Successfully deleted contact {contact_id}");
-
-    Ok(())
-}
-
-pub async fn import_contacts(
-    command: &ImportCommand,
-    data_repo: &db::Connection,
-) -> Result<(), anyhow::Error> {
-    let result_of_import = data_repo
-        .import_contacts_by_csv(command.filename.as_str())
-        .await;
-
-    match result_of_import {
-        Ok(number_of_imports) => {
-            println!("Successfully imported {number_of_imports} contact");
-        }
-        Err(error) => {
-            println!("{error}");
-        }
+        Ok(())
     }
 
-    Ok(())
+    pub async fn edit_contact(&self, command: &EditCommand) -> Result<(), anyhow::Error> {
+        let contact = ContactBuilder::new(
+            command.id,
+            command.first_name.clone(),
+            command.last_name.clone(),
+            command.display_name.clone(),
+            command.email.clone(),
+            command.phone_number.clone(),
+        )
+        .unwrap();
+
+        let _ = self.data_repo.update_contact(contact).await;
+
+        Ok(())
+    }
+
+    pub async fn show_all_contacts(&self) -> Result<(), anyhow::Error> {
+        let contacts = self.data_repo.get_all_contacts().await?;
+
+        if contacts.is_empty() {
+            println!("No contacts yet!");
+        } else {
+            let table = Table::new(contacts);
+            println!("{table}");
+        }
+
+        Ok(())
+    }
+
+    pub async fn get_contact(&self, command: &GetCommand) -> Result<(), anyhow::Error> {
+        let id = command.id;
+
+        let contact = self.data_repo.get_contact_by_id(id).await?;
+
+        println!("{contact:?}");
+
+        Ok(())
+    }
+
+    pub async fn delete_contact(&self, command: &DeleteCommand) -> Result<(), anyhow::Error> {
+        let id = command.id;
+
+        let contact_id = self.data_repo.delete_contact_by_id(id).await?;
+
+        println!("Successfully deleted contact {contact_id}");
+
+        Ok(())
+    }
+
+    pub async fn import_contacts(&self, command: &ImportCommand) -> Result<(), anyhow::Error> {
+        let result_of_import = self
+            .data_repo
+            .import_contacts_by_csv(command.filename.as_str())
+            .await;
+
+        match result_of_import {
+            Ok(number_of_imports) => {
+                println!("Successfully imported {number_of_imports} contact");
+            }
+            Err(error) => {
+                println!("{error}");
+            }
+        }
+
+        Ok(())
+    }
 }
--- src/cli/main.rs
+++ src/cli/main.rs
@@ -1,13 +1,12 @@
 use std::env;
 
-use db::Connection;
-use nbd::db;
-
 mod actions;
 mod commander;
 
+use actions::Actions;
 use clap::Parser;
 use commander::{Cli, Commands};
+use nbd::db::Connection;
 use sqlx::SqlitePool;
 
 #[tokio::main]
@@ -15,18 +14,18 @@ async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().ok();
 
     let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
-
     let data_repo = Connection::new(pool);
+    let actions = Actions::new(data_repo);
 
     let cli = Cli::parse();
 
     match &cli.command {
-        Commands::Create(value) => actions::create_contact(value, &data_repo).await?,
-        Commands::Edit(value) => actions::edit_contact(value, &data_repo).await?,
-        Commands::Show => actions::show_all_contacts(&data_repo).await?,
-        Commands::Get(value) => actions::get_contact(value, &data_repo).await?,
-        Commands::Delete(value) => actions::delete_contact(value, &data_repo).await?,
-        Commands::Import(value) => actions::import_contacts(value, &data_repo).await?,
+        Commands::Create(value) => actions.create_contact(value).await?,
+        Commands::Edit(value) => actions.edit_contact(value).await?,
+        Commands::Show => actions.show_all_contacts().await?,
+        Commands::Get(value) => actions.get_contact(value).await?,
+        Commands::Delete(value) => actions.delete_contact(value).await?,
+        Commands::Import(value) => actions.import_contacts(value).await?,
     }
 
     Ok(())
