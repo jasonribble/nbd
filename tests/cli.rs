75cc53a6cdbc5e91e9ed62e3efbd09da1768d4bf
Jason Ribble <git@j6r.org>
2025-04-13 12:02:37 -0700

    test(cli): improve test reliability
    better names and less flaky

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,7 +1,8 @@
 #[cfg(test)]
-
 mod tests {
     use assert_cmd::Command;
+    use nbd::db::ContactRepo;
+    use nbd::{db::Connection, models::Contact};
     use sqlx::SqlitePool;
 
     fn create_command() -> Command {
@@ -44,25 +45,40 @@ mod tests {
     }
 
     #[test]
-    fn test_cli_name() {
+    fn should_have_name_nbd_cli() {
         let crate_name = get_cli_name();
         assert_eq!(crate_name, "nbd-cli");
     }
 
     #[test]
-    fn test_cli_help() {
+    fn should_display_cli_help() {
         let mut cmd = create_command();
         cmd.arg("--help");
 
-        let stdout = format!("Usage: {} <COMMAND>", get_cli_name());
+        let expected_output = vec![
+            "Usage: nbd-cli <COMMAND>",
+            "",
+            "Commands:",
+            "  create  Create a contact",
+            "  edit    Edit a contact by ID",
+            "  show    Get all contacts",
+            "  get     Get a contact",
+            "  delete  Delete a contact",
+            "  import  Import contact via CSV",
+            "  help    Print this message or the help of the given subcommand(s)",
+            "",
+            "Options:",
+            "  -h, --help     Print help",
+            "  -V, --version  Print version",
+        ];
 
         cmd.assert()
             .success()
-            .stdout(predicates::str::contains(stdout));
+            .stdout(predicates::str::contains(expected_output.join("\n")));
     }
 
-    #[test]
-    fn test_cli_create_contact() {
+    #[tokio::test]
+    async fn should_be_able_to_create_full_contact() {
         let mut cmd = create_command();
         cmd.arg("create")
             .arg("--first-name")
@@ -77,39 +93,43 @@ mod tests {
         cmd.assert()
             .success()
             .stdout(predicates::str::contains("Successfully saved contact"));
+
+        clean_database().await.unwrap();
     }
 
-    #[test]
-    fn test_cli_delete_contact() {
+    #[tokio::test]
+    async fn should_delete_a_contact_when_one_is_present() -> anyhow::Result<()> {
+        let pool = SqlitePool::connect("sqlite:contacts.db").await?;
+        let data_repo = Connection::new(pool);
+
+        let birthday = chrono::NaiveDate::from_ymd_opt(1832, 1, 27).unwrap();
+
+        let example_contact = Contact::new(
+            "Lewis",
+            "Carroll",
+            "lewis@wonderland.com",
+            "777-777-7777",
+            birthday,
+        )
+        .unwrap();
+
+        data_repo.save_contact(example_contact).await.unwrap();
+
         let mut cmd = create_command();
-
-        let create_output = cmd
-            .arg("create")
-            .arg("--first-name")
-            .arg("First")
-            .arg("--last-name")
-            .arg("Last")
-            .arg("--email")
-            .arg("test@test.com")
-            .arg("--phone-number")
-            .arg("123-321-1233");
-
-        let output = create_output.output().unwrap();
-
-        let stdout = String::from_utf8(output.stdout).unwrap().trim().to_string();
-        let contact_id = stdout.chars().last().unwrap().to_string();
-
-        cmd = create_command();
-
+        let contact_id = "1";
         cmd.arg("delete").arg(contact_id);
 
         cmd.assert()
             .success()
             .stdout(predicates::str::contains("Successfully deleted contact"));
+
+        clean_database().await?;
+
+        Ok(())
     }
 
     #[test]
-    fn test_cli_invalid_email() {
+    fn should_error_when_providing_invalid_email() {
         let mut cmd = create_command();
         cmd.arg("create")
             .arg("--first-name")
@@ -127,7 +147,7 @@ mod tests {
     }
 
     #[test]
-    fn test_cli_invalid_phone() {
+    fn should_error_when_providing_invalid_phone_number() {
         let mut cmd = create_command();
         cmd.arg("create")
             .arg("--first-name")
@@ -145,7 +165,7 @@ mod tests {
     }
 
     #[test]
-    fn test_cli_invalid_args() {
+    fn should_error_when_invalid_args() {
         let mut cmd = create_command();
         cmd.arg("First").arg("Last").arg("32321123");
 
@@ -157,23 +177,26 @@ mod tests {
     }
 
     #[tokio::test]
-    #[ignore = "TODO: fix acceptance tests"]
     async fn should_import_one_contact_when_importing_alice_csv() -> anyhow::Result<()> {
-        clean_database().await?;
         let mut cmd = create_command();
         cmd.arg("import").arg("tests/fixtures/alice.csv");
 
-        let mut cmd = create_command();
-        cmd.arg("show");
-
         cmd.assert()
             .success()
-            .stdout(predicates::str::contains("Alice"));
+            .stdout(predicates::str::contains("Successfully imported 1 contact"));
+
+        let pool = SqlitePool::connect("sqlite:contacts.db").await?;
+        let data_repo = Connection::new(pool);
+
+        let contacts = data_repo.get_all_contacts().await?;
+
+        assert_eq!(contacts.len(), 1);
+
         Ok(())
     }
 
     #[test]
-    fn test_import() {
+    fn should_import_example_csv_with_three_rows() {
         let mut cmd = create_command();
         cmd.arg("import").arg("tests/fixtures/example.csv");
 
@@ -290,8 +313,8 @@ mod tests {
         Ok(())
     }
 
-    #[test]
-    fn should_accept_a_firstname_and_birthday() {
+    #[tokio::test]
+    async fn should_accept_a_firstname_and_birthday() {
         let mut cmd = create_command();
         cmd.arg("create")
             .arg("--first-name")
@@ -302,5 +325,7 @@ mod tests {
         cmd.assert()
             .success()
             .stdout(predicates::str::contains("Successfully saved contact"));
+
+        clean_database().await.unwrap();
     }
 }
